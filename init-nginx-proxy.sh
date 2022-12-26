13ff48657c00ea97ae5eedcaea6b576be5dcd4d4
mapned <110787440+mapned@users.noreply.github.com>
2022-12-26 22:41:52 +0200

    Update init-nginx-proxy.sh

--- init-nginx-proxy.sh
+++ init-nginx-proxy.sh
@@ -1,8 +1,13 @@
 #/bin/bash
+echo
 if [ -z "$1" ] || [ "$1" = "" ]
 then
-	echo "Missing CLI parameter for proxy domain name. Give domain name as parameter, for ex.: ./init-nginx-proxy.sh domain-name.com
+	echo "Undefined proxy domain name. Give domain name as parameter, for ex.: ./init-nginx-proxy.sh domain-name.com
+	exit 1
 fi
+
+echo "Initializing nginx proxy for Guacamole using $1 as domain name"
 sudo cp nginx/mysite.template /etc/nginx/conf.d/guacamole-proxy.conf
 sudo sed -i "s|domain-name-here.com|$1|g" /etc/nginx/conf.d/guacamole-proxy.conf
-echo "Nginx proxy for Guacamole initialized. You'll want to create and deploy certificates (with certbot or similar) and restart nginx"
+echo
+echo "Done: nginx proxy for Guacamole initialized. You'll want to create and deploy certificates (with certbot or similar) and restart nginx"
